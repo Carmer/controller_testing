174df2db62c0f612e87b4b10274f6d5beebc974d
Andrew Carmer <carmer@Andrews-MacBook-Air.local>
2016-05-18 10:47:47 -0600

    update controller

--- app/controllers/to_do_app.rb
+++ app/controllers/to_do_app.rb
@@ -3,20 +3,16 @@ class ToDoApp < Sinatra::Base
   post '/items' do
     item = Item.new(params[:item])
     if item.save
-      response.status = 200
-      response.body = "Item created"
+      status  200
+      body  "Item created"
     else
-      response.status = 400
-      response.body = item.errors.full_messages.join(", ")
+      status  400
+      body  item.errors.full_messages.join(", ")
     end
   end
 
   get '/items' do
-    response.body = Item.all.to_json
-    response.status = 200
+    body  Item.all.to_json
+    status  200
   end
-
-
-
-
 end
